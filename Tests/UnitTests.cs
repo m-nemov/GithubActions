8a4b7f425a553923246c94b7e5368dc549707e74
mnemov2 <mnemov2@unl.edu>
2025-03-30 13:42:18 -0500

    Add unit tests for Subtract, Multiply, Divide, and Power

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -27,3 +27,99 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
 	}
 }
+
+public class SubtractTests
+{
+    [TestMethod]
+    public void Subtract_Valid_Patino()
+    {
+        Assert.AreEqual(1, Program.Subtract("3", "2"));
+        Assert.AreEqual(-5, Program.Subtract("2", "7"));
+    }
+
+    [TestMethod]
+    public void Subtract_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
+    }
+
+    [TestMethod]
+    public void Subtract_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null!));
+    }
+}
+
+[TestClass]
+public class MultiplyTests
+{
+    [TestMethod]
+    public void Multiply_Valid_Patino()
+    {
+        Assert.AreEqual(6, Program.Multiply("2", "3"));
+        Assert.AreEqual(0, Program.Multiply("5", "0"));
+    }
+
+    [TestMethod]
+    public void Multiply_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Multiply("x", "3"));
+    }
+
+    [TestMethod]
+    public void Multiply_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null!, "3"));
+    }
+}
+
+[TestClass]
+public class DivideTests
+{
+    [TestMethod]
+    public void Divide_Valid_Patino()
+    {
+        Assert.AreEqual(2, Program.Divide("10", "5"));
+    }
+
+    [TestMethod]
+    public void Divide_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Divide("10", "y"));
+    }
+
+    [TestMethod]
+    public void Divide_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null!, "2"));
+    }
+
+    [TestMethod]
+    public void Divide_By_Zero()
+    {
+        Assert.ThrowsException<DivideByZeroException>(() => Program.Divide("10", "0"));
+    }
+}
+
+[TestClass]
+public class PowerTests
+{
+    [TestMethod]
+    public void Power_Valid_Patino()
+    {
+        Assert.AreEqual(8, Program.Power("2", "3"));
+        Assert.AreEqual(1, Program.Power("5", "0"));
+    }
+
+    [TestMethod]
+    public void Power_Invalid_Patino()
+    {
+        Assert.ThrowsException<FormatException>(() => Program.Power("2", "a"));
+    }
+
+    [TestMethod]
+    public void Power_Null_Patino()
+    {
+        Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null!, "3"));
+    }
+}
