00cbaf7c0c061d0097e17270704002ccf761da25
mnemov2 <mnemov2@unl.edu>
2025-03-30 14:08:34 -0500

    Rename all test methods to include last name Nemov

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -4,7 +4,7 @@ namespace GithubActionsLab;
 public class Addition
 {
 	[TestMethod]
-	public void Add_Valid_Patino()
+	public void Add_Valid_Nemov()
 	{
 		Assert.AreEqual(3, Program.Add("1", "2"));
 		Assert.AreEqual(5, Program.Add("3", "2"));
@@ -12,7 +12,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Invalid_Patino()
+	public void Add_Invalid_Nemov()
 	{
 		Assert.ThrowsException<FormatException>(() => Program.Add("1", "a"));
 		Assert.ThrowsException<FormatException>(() => Program.Add("a", "1"));
@@ -20,7 +20,7 @@ public class Addition
 	}
 
 	[TestMethod]
-	public void Add_Null_Patino()
+	public void Add_Null_Nemov()
 	{
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,20 +31,20 @@ public class Addition
 public class SubtractTests
 {
     [TestMethod]
-    public void Subtract_Valid_Patino()
+    public void Subtract_Valid_Nemov()
     {
         Assert.AreEqual(1, Program.Subtract("3", "2"));
         Assert.AreEqual(-5, Program.Subtract("2", "7"));
     }
 
     [TestMethod]
-    public void Subtract_Invalid_Patino()
+    public void Subtract_Invalid_Nemov()
     {
         Assert.ThrowsException<FormatException>(() => Program.Subtract("1", "a"));
     }
 
     [TestMethod]
-    public void Subtract_Null_Patino()
+    public void Subtract_Null_Nemov()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Subtract("1", null!));
     }
@@ -54,20 +54,20 @@ public class SubtractTests
 public class MultiplyTests
 {
     [TestMethod]
-    public void Multiply_Valid_Patino()
+    public void Multiply_Valid_Nemov()
     {
         Assert.AreEqual(6, Program.Multiply("2", "3"));
         Assert.AreEqual(0, Program.Multiply("5", "0"));
     }
 
     [TestMethod]
-    public void Multiply_Invalid_Patino()
+    public void Multiply_Invalid_Nemov()
     {
         Assert.ThrowsException<FormatException>(() => Program.Multiply("x", "3"));
     }
 
     [TestMethod]
-    public void Multiply_Null_Patino()
+    public void Multiply_Null_Nemov()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Multiply(null!, "3"));
     }
@@ -77,26 +77,26 @@ public class MultiplyTests
 public class DivideTests
 {
     [TestMethod]
-    public void Divide_Valid_Patino()
+    public void Divide_Valid_Nemov()
     {
         Assert.AreEqual(2, Program.Divide("10", "5"));
     }
 
     [TestMethod]
-    public void Divide_Invalid_Patino()
+    public void Divide_Invalid_Nemov()
     {
         Assert.ThrowsException<FormatException>(() => Program.Divide("10", "y"));
     }
 
     [TestMethod]
-    public void Divide_Null_Patino()
+    public void Divide_Null_Nemov()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Divide(null!, "2"));
     }
 
     [TestMethod]
     
-	public void Divide_By_Zero()
+	public void Divide_By_Zero_Nemov()
 	{
 		var result = Program.Divide("10", "0");
 		Assert.IsTrue(double.IsInfinity(result));
@@ -107,20 +107,20 @@ public class DivideTests
 public class PowerTests
 {
     [TestMethod]
-    public void Power_Valid_Patino()
+    public void Power_Valid_Nemov()
     {
         Assert.AreEqual(8, Program.Power("2", "3"));
         Assert.AreEqual(1, Program.Power("5", "0"));
     }
 
     [TestMethod]
-    public void Power_Invalid_Patino()
+    public void Power_Invalid_Nemov()
     {
         Assert.ThrowsException<FormatException>(() => Program.Power("2", "a"));
     }
 
     [TestMethod]
-    public void Power_Null_Patino()
+    public void Power_Null_Nemov()
     {
         Assert.ThrowsException<ArgumentNullException>(() => Program.Power(null!, "3"));
     }
